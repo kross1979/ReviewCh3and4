1c8650f5688d6fb8b3ce0bb48f682a72db31d416
0273898 <0273898@KELCE-LABS13-P.pittstate.edu>
2019-09-17 11:31:11 -0500

    end of class commit

--- src/main/java/Review.java
+++ src/main/java/Review.java
@@ -1,3 +1,8 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,7 +14,29 @@
  * @author 0273898
  */
 public class Review {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Please enter file name");
+        
+        Scanner keyboard = new Scanner (System.in);
+        String fileName = keyboard.nextLine();
+        
+        File myFile = new File("C://users//johnk//desktop//" + fileName);
+        
+
+
+
+        Scanner fileScanner = new Scanner(myFile);
+        
+        int counter = 0;
+        while (fileScanner.hasNext() && counter <5) {
+             System.out.println(fileScanner.nextLine());
+             counter++;
+        }
+        fileScanner.close();
+         }
+        
+        
+       
         
     }
 }
